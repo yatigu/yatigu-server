1d514bddf94d5b062c5470663bcd72674f4df176
yeonsooKim <gjb2628@gmail.com>
2020-01-28 00:39:19 +0900

    로그인 전용 드라이버 추가

--- login/views.py
+++ login/views.py
@@ -1,7 +1,13 @@
 from flask.views import MethodView
+from selenium import webdriver
+
+
+driver = webdriver.Chrome('C:\chromedriver.exe')  # 로그인 전용으로 미리 열어놓는 크롬드라이버
+
 
 class Login(MethodView):
     def get(self):
+        driver.get('http://www.naver.com/')  # 샘플코드. 컨트롤 가능한지 테스트.
         return 'get'
 
     def post(self):
