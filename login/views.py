34bbe35131c7b1d5cfa6bd56f806d0272dbd515b
yeonsooKim <gjb2628@gmail.com>
2020-02-02 15:24:51 +0900

    테스트 크롬드라이버 제거

--- login/views.py
+++ login/views.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from settings.settings import CHROME_DRIVER_PATH
 
 
-driver = webdriver.Chrome(CHROME_DRIVER_PATH)  # 로그인 전용으로 미리 열어놓는 크롬드라이버
+# driver = webdriver.Chrome(CHROME_DRIVER_PATH)  # 로그인 전용으로 미리 열어놓는 크롬드라이버
 
 
 class Login(MethodView):
