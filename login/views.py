607a3b61cbcfa8d86a05415fa6c9cfda257807c0
yeonsooKim <gjb2628@gmail.com>
2020-02-02 15:08:16 +0900

    크롬드라이버 패스 변경

--- login/views.py
+++ login/views.py
@@ -1,8 +1,9 @@
 from flask.views import MethodView
 from selenium import webdriver
+from settings.settings import CHROME_DRIVER_PATH
 
 
-driver = webdriver.Chrome('C:\chromedriver.exe')  # 로그인 전용으로 미리 열어놓는 크롬드라이버
+driver = webdriver.Chrome(CHROME_DRIVER_PATH)  # 로그인 전용으로 미리 열어놓는 크롬드라이버
 
 
 class Login(MethodView):
