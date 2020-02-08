67f137d92e4837c56522986936c2e19a71c1091b
yeonsooKim <gjb2628@gmail.com>
2020-02-09 00:52:42 +0900

    표정보 리퀘스트 생성

--- crawling/tickets.py
+++ crawling/tickets.py
@@ -1,7 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
 
-def tickets(html):
-    bs = BeautifulSoup(html, 'lxml')
-    tickets = bs.find_all('tbody')
+def tickets():
+    params = {
+        'selGoTrain': '05',
+        'radJobId': '1',
+        'checkStnNm': 'Y',
+        'txtGoAbrdDt': '20200210',
+        'txtGoHour': '160000',
+        'txtGoStart': '서울',
+        'txtGoEnd': '부산',
 
+    }
+    res = requests.post('http://www.letskorail.com/ebizprd/EbizPrdTicketPr21111_i1.do', params=params)
+    print(res.text)
+
+tickets()
