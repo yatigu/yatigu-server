c397ecdf319cfb54c5ea15578e45fca0159ac3c9
yeonsooKim <gjb2628@gmail.com>
2020-02-09 18:32:18 +0900

    기차표조회 url 추가

--- info/urls.py
+++ info/urls.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
-from .views import Station
+from .views import Stations, Tickets
+
+
 app = Blueprint('info', __name__)
 
-station_view = Station.as_view('info_view')
+station_view = Stations.as_view('stations_view')
+tickets_view = Tickets.as_view('tickets_view')
 app.add_url_rule('/stations/', view_func=station_view)
+app.add_url_rule('/tickets/', view_func=tickets_view)
