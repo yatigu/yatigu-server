93d6541f1b05b5d895012a7fb1beb6227f3161c1
yeonsooKim <gjb2628@gmail.com>
2020-02-09 18:54:23 +0900

    불필요제거

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
-from .views import AccountUser
 app = Blueprint('account', __name__)
 
-login_view = AccountUser.as_view('account_user_view')
-app.add_url_rule('/user/', view_func=login_view)
+#login_view = AccountUser.as_view('account_user_view')
+#app.add_url_rule('/user/', view_func=login_view)
