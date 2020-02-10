00ad57589a2f127e8472b76592188a3e40c5dce1
yeonsooKim <gjb2628@gmail.com>
2020-02-10 23:40:03 +0900

    test

--- account/views.py
+++ account/views.py
@@ -46,7 +46,7 @@ class Hooks(MethodView):
         try:
             res = json.loads(request.form)
             print(res['ref'])
-            if res['ref'] == 'refs/heads/tickets':
+            if res['ref'] == 'refs/heads/master':
                 print(res['ref'])
                 os.system('sh /home/ec2-user/yatigu-server/settings/hooks.sh')
         except Exception as e:
