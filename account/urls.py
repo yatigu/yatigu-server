bced8a98b0a11aca86eb0b6789940037a072dc74
yeonsookim <gjb2628@gmail.com>
2020-02-10 17:01:10 +0900

    add hooks

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+
+from account.views import Hooks
+
 app = Blueprint('account', __name__)
 
 #login_view = AccountUser.as_view('account_user_view')
 #app.add_url_rule('/user/', view_func=login_view)
+hooks_view = Hooks.as_view('hooks_view')
+app.add_url_rule('/hooks/', view_func=hooks_view)
--- account/views.py
+++ account/views.py
@@ -36,3 +36,7 @@ class AccountUser(MethodView):
             user_list.append(get_user)  # 유저 리스트에 해당 유저를 추가한다.
         return jsonify(serialize(get_user)), 200
 '''
+class Hooks:
+    def post(self):
+        import os
+        os.system('/home/ec2-user/yatigu-server/.git/hooks/post-receive')
