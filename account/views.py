c892b16b62e1dd39399f62ee3dbe40177d032750
yeonsooKim <gjb2628@gmail.com>
2020-02-10 23:41:45 +0900

    test

--- account/views.py
+++ account/views.py
@@ -46,6 +46,7 @@ class Hooks(MethodView):
 
         print(request.headers)
         print(request.form['ref'])
+        os.system('sh /home/ec2-user/yatigu-server/settings/hooks.sh')
 
 
         return Response('push', status=200)
