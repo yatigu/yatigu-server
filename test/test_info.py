4612fd3fe58a7fbd43dd80bc43d7bf09c5cdc363
yeonsooKim <gjb2628@gmail.com>
2020-02-17 23:01:08 +0900

    add init

--- test/test_info.py
+++ test/test_info.py
@@ -1,13 +1,12 @@
 from datetime import datetime
-from settings.utils import client
 
 
-def test_stations():  # 기차역을 불러온다.
+def test_stations(client):  # 기차역을 불러온다.
     res = client.get('/info/stations/')
     assert res.status_code == 200
 
 
-def test_tickets():  # 티켓 정보를 불러온다.
+def test_tickets(client):  # 티켓 정보를 불러온다.
     data = {
         'date': str(datetime.now()).replace('-', '')[0:8],
         'hour': '235900',
