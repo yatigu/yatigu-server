d65464ae1d65b042b77fddc00fc332d7d5b233e5
yeonsooKim <gjb2628@gmail.com>
2020-02-25 00:04:18 +0900

    decorator 추가

--- info/views.py
+++ info/views.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from werkzeug.exceptions import BadRequestKeyError
 from crawling.stations import get_stations
 from crawling.tickets import get_tickets
+from settings.utils import api
 
 
 class Stations(MethodView):
@@ -16,19 +17,19 @@ class Stations(MethodView):
 
 
 class Tickets(MethodView):
-    def get(self):
-        res = request.form
+    @api
+    def get(self, data):
         keys = list()  # key 검사
-        for key in res.keys():
+        for key in data.keys():
             keys.append(key)
         if 'date' and 'hour' and 'start' and 'end' not in keys:
             raise BadRequestKeyError  # key가 올바르지 않음
         return jsonify(get_tickets(request.form['date'], request.form['hour'], request.form['start'], request.form['end']))
 
-    def post(self):
-        res = request.form
+    @api
+    def post(self, data):
         keys = list()  # key 검사
-        for key in res.keys():
+        for key in data.keys():
             keys.append(key)
         if 'date' and 'hour' and 'start' and 'end' not in keys:
             raise BadRequestKeyError  # key가 올바르지 않음
