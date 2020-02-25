dd7ffc9cfffeda230a0868786c3c8d5e72d7e196
yeonsookim <gjb2628@gmail.com>
2020-02-26 00:19:15 +0900

    테스트케이스 수정

--- test/test_info.py
+++ test/test_info.py
@@ -15,7 +15,7 @@ def test_tickets(client):  # 티켓 정보를 불러온다.
     }
     res = client.get('/info/tickets/')  # data 없거나 안맞을경우
     assert res.status_code == 400
-    res = client.get('/info/tickets/', data=data)
+    res = client.get('/info/tickets/', query_string=data)
     assert res.status_code == 200
 
 
