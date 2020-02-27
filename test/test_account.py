3a7706f328c4a87829e890fd204f9bcbc7371242
yeonsooKim <gjb2628@gmail.com>
2020-02-28 00:46:39 +0900

    /account/user/ 테스트코드 추가

---
+++ test/test_account.py
@@ -0,0 +1,16 @@
+def test_user(client):
+    res = client.post('/account/user/')
+    assert res.status_code == 400
+
+    data = {
+        'id': '01026282628',
+        'pw': '01026282628',
+        'index': '3',
+        'source': '서울',
+        'destination': '부산',
+        'year': '2020',
+        'month': '03',
+        'day': '09'
+    }
+    res = client.post('/account/user/', data=data)
+    assert res.status_code == 200
