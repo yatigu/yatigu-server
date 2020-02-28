2da9a97e29f601e35ee3d1e7683b9709ec1afc01
yeonsooKim <gjb2628@gmail.com>
2020-02-28 20:06:00 +0900

    headless 추가

--- practice/macro.py
+++ practice/macro.py
@@ -37,7 +37,7 @@ class Korail:  # 코레일 매크로 클래스
         alert.accept()
     def chrome_options(self):  # 크롬 드라이버에 옵션을 추가하는 함수
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")  # 창 안띄우고 진행
+        chrome_options.add_argument("--headless")  # 창 안띄우고 진행
         chrome_options.add_argument("disable-infobars")  # info message 안띄움
         chrome_options.add_argument("start-maximized")  # 나머지 옵션은 잘 모르겠는데 빨라진다는 말이있어서 씀
         chrome_options.add_argument("--disable-extensions")
